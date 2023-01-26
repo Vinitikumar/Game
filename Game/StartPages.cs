b128818c6cc07c4e85e60205ffb80e07fa64531a
Vinitikumar <94457045+Vinitikumar@users.noreply.github.com>
2023-01-26 13:59:56 +0100

    Added Tabpages

---
+++ Game/StartPages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class StartPages : Form
+    {
+        public StartPages()
+        {
+            InitializeComponent();
+        }
+
+        private void singlePlayer_Click(object sender, EventArgs e)
+        {
+            GameScreenForSingle singlePlayerPage = new GameScreenForSingle();
+            singlePlayerPage.Show();
+        }
+
+        private void startButton_Click_1(object sender, EventArgs e)
+        {
+            tabController.SelectedTab = PlayerChoicesPage;
+        }
+    }
+}
